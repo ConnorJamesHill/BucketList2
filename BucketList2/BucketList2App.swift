a4348a3558a23ba63ae84e226b148ecfdbb489b5
Meruem Moniker <jamesconnorhill@gmail.com>
2024-03-11 19:39:04 -0400

    Initial Commit

---
+++ BucketList2/BucketList2App.swift
@@ -0,0 +1,17 @@
+//
+//  BucketList2App.swift
+//  BucketList2
+//
+//  Created by Connor Hill on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
